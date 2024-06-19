1452fe6db8e164ee0b115cba79b79620433437bd
byp3f3 <nekrasovaanastasia790@gmail.com>
2024-06-19 17:56:37 +0300

    photoapp

---
+++ app/src/main/java/com/example/myapplication/FullImageActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.InputStream;
+
+public class FullImageActivity extends AppCompatActivity {
+
+    ImageView fullImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_full_image);
+        fullImage = findViewById(R.id.full_image);
+
+        String imageUriString = getIntent().getStringExtra("image");
+        Uri imageUri = Uri.parse(imageUriString);
+        try{
+            InputStream imageStream = getContentResolver().openInputStream(imageUri);
+            Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+            fullImage.setImageBitmap(selectedImage);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
